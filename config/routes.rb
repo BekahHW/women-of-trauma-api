daa449e084da9f14a5c5d1a5779e922fe916ea69
github username <github email address>
2019-05-02 06:00:47 -0400

    Add /api/narratives and /api/disorders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :narratives, except: [:new]
+  end
+
+  namespace :api do
+    resources :disorders, except: [:new, :edit, :delete, :patch, :put, :create, :update, :destroy]
+  end
 end
