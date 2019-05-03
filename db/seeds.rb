6474ddf0d2901c157edd21c93c53c0e8ac5b3b5b
github username <github email address>
2019-05-03 05:59:57 -0400

    Create Disorder seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Disorder.create(name: 'Natural Disasters', description: 'Any natural catastrophe (e.g., tornadoes, hurricanes, and earthquakes) that causes enough damage that local, state, or federal agencies and disaster relief organizations are called into action.')
+
+Disorder.create(name: 'Traumatic Loss', description: 'Traumatic loss or grief can occur following a death of someone important to a child or adult. The death is typically sudden and unexpected.')
+
+Disorder.create(name: 'Medical Trauma', description: 'Children may show traumatic reactions to medical conditions, invasive medical procedures, or treatments that are frightening or cause pain, injury, and/or serious illness.')
+
+Disorder.create(name: 'School and Community Violence', description: 'School and community violence include predatory violence or personal conflicts between people who are not family members (e.g., shootings, rape, robbery).')
+
+
+Disorder.create(name: "War-related Trauma", description: "Refugee and warzone trauma. Terrorism. Combat-related trauma.")
+
+Disorder.create(name: "Sexual Assault", description: "Sexual assault involves any unwanted and involuntary sexual behavior towards a person. The victim is forced or coerced engage in an act against their will in a non-consensual setting. Sexual assault can include rape, groping, forced kissing, and any other sort of harassment or abuse in a sexual context.")
+
+Disorder.create(name: "Domestic Violence", description: "Domestic violence is classified as actual or threatened physical violence, sexual violence, and/or emotional abuse between adults in an intimate relationship.")
+
+Disorder.create(name: "Child Maltreatment", description: "Neglect. Physical abuse. Mental abuse. Maltreatment. Sexual Abuse." )
