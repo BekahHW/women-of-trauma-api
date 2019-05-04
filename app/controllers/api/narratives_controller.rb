7d64a7e1a67e7396f2927e8ff6bcf09c358406af
github username <github email address>
2019-05-04 05:53:48 -0400

    Finish Rails API Narratives controller

--- app/controllers/api/narratives_controller.rb
+++ app/controllers/api/narratives_controller.rb
@@ -1,4 +1,5 @@
 class Api::NarrativesController < ApplicationController
+  before_action :set_narrative[:update, :show, :destroy] 
 
   def index
     render json: Narrative.all
@@ -14,10 +15,30 @@ class Api::NarrativesController < ApplicationController
   end
 
   def show
-    render json: Narrative.find_by(id: params[:id])
+    render json: @narrative
+  end
+
+  def update
+    if @narrative.update(narrative_params)
+      render json: @narrative
+    else
+      render json: {message: narrative.errors}, status: 400
+    end
+  end
+
+  def destroy
+    if @narrative.destroy(narrative_params)
+      render status: 204
+    else
+      render json: {message: "Cannot delete story at this time."}, status: 400
+    end
   end
 
   private
+
+  def set_narrative
+    @narrative = Narrative.find_by(id: params[:id])
+  end
 
    def narrative_params
      params.require(:narrative).permit(:title, :tagline, :story, :user_id, :trauma_id)
